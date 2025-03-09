d3f19430c62770257abde354005f731efb9970ab
Khezy Gwen Mangubat <khezmangubat@gmail.com>
2025-03-08 19:43:12 -0600

    Add files via upload

---
+++ api/config/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = "localhost:3307";
+    private $db_name = "student_db";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection error: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
